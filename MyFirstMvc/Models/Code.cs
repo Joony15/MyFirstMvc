4ec68854929cdd1728bd4fe17d00ac83497872a5
Joon-h <noddykk@naver.com>
2016-05-27 10:41:05 +0900

    Add project files.

---
+++ MyFirstMvc/Models/Code.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace MyFirstMvc.Models
+{
+    public class Code
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+     public class CodeDBContext : DbContext
+    {
+        public DbSet<Code> Codes { get; set; }
+    }
+}
